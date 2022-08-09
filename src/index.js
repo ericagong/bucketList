9192b61f8927fd4ab265a5b17f4a5da2f739a471
ericagong <48196721+ericagong@users.noreply.github.com>
2022-08-09 10:34:43 +0900

    add theme

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/configStore/store";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./Theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <ThemeProvider theme={darkTheme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
