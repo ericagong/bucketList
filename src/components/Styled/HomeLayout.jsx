24058418d4f93b880fe3fb3f12024d793cbf0d3b
ericagong <48196721+ericagong@users.noreply.github.com>
2022-08-09 15:33:19 +0900

    HomeLayout

--- src/components/Styled/HomeLayout.jsx
+++ src/components/Styled/HomeLayout.jsx
@@ -1,13 +1,32 @@
+import styled from "styled-components";
+
 import Category from "../Post/Category";
 
 const HomeLayout = (props) => {
   return (
-    <>
-      Move to each category!
-      <Category contents='Create New Post' path='/create' />
-      <Category contents='Read Posts' path='/posts' />
-    </>
+    <Wrapper>
+      <Helper>[Move to each category!]</Helper>
+      <Category contents='📝 Create New Post' path='/create' />
+      <Category contents='📖 Read Posts' path='/posts' />
+    </Wrapper>
   );
 };
 
 export default HomeLayout;
+
+const Wrapper = styled.div`
+  width: 70vw;
+  height: 80vh;
+  max-width: 1000px;
+  max-height: 700px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Helper = styled.h3`
+  height: 10%;
+  padding-top: 20px;
+  font-weight: bold;
+`;
