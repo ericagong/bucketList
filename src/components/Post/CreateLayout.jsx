12c6e6d7976e6dd64de1ed19e7ec2095894d3b6d
kksltv123 <kksltv123@naver.com>
2022-08-10 14:00:26 +0900

    add reac-hook-form in CreateLayout

--- src/components/Post/CreateLayout.jsx
+++ src/components/Post/CreateLayout.jsx
@@ -13,62 +13,52 @@ import { useForm } from "react-hook-form";
 const CreateLayout = (props) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { register, watch, formState: { errors }, handleSubmit } = useForm();
-  console.log(watch('name'))
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const [username, setUsername] = useState("");
-  const [title, setTitle] = useState("");
-  const [contents, setContents] = useState("");
+  // 공백 문자
+  let black_pattern = /^\s+|\s+$/g;
 
-  const onChangeHandler = (e) => {
-    const id = e.target.id;
-    const value = e.target.value;
-    if (!value) {
-      return;
+  // data 객체에 username, title, contents 값 포함
+  const onSubmit = (data) => {
+    if (data.username.replace(black_pattern, '') === "" ) {
+      alert("Only spaces were entered in the username")
+      return
+    } else if (data.title.replace(black_pattern, '') === "" ) {
+      alert("Only spaces were entered in the title")
+      return
+    } else if (data.contents.replace(black_pattern, '') === "" ) {
+      alert("Only spaces were entered in the contents")
+      return
     } else {
-      if (id === "username") setUsername(value);
-      else if (id === "title") setTitle(value);
-      else setContents(value);
+      dispatch(__createPost(data));
+      navigate("/posts");
     }
-  };
+  }
+  const onError = (errors, e) => console.log(errors, e);
+  // 마스터 브랜치
 
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    if (!username || !title || !contents) {
-      return;
-    }
-    // json-server automatically creates ID.
-    const new_post = {
-      username,
-      title,
-      contents,
-    };
-    dispatch(__createPost(new_post));
-    navigate("/posts");
-  };
+return (
+  <div>
+    <StForm onSubmit={handleSubmit(onSubmit, onError)}>
+      <label htmlFor='username'>Username</label>
+      <input type='text' id='username' placeholder="10 characters or less" {...register("username", { required: true, maxLength: 10, })} />
+      {errors.username && errors.username.type === "required" && <p>This field is required</p>}
+      {errors.username && errors.username.type === "maxLength" && <p>Your name exceed maximum length</p>}
 
-  return (
-    <div>
-      <StForm onSubmit={onSubmitHandler}>
-        <label htmlFor='username'>Username</label>
-        <input name="name" type='text' id='username' onChange={onChangeHandler} {...register("name", { required: true, maxLength: 10 })}/>
-        {errors.name && errors.name.type === "required"&&<p>This field is required</p>}
-        {errors.name && errors.name.type === "maxLength"&&<p>Your name exceed maximum length</p>}
+      <label htmlFor='title'>Title</label>
+      <input type='text' id='title' placeholder="20 characters or less" {...register("title", { required: true, maxLength: 20 })} />
+      {errors.title && errors.title.type === "required" && <p>This field is required</p>}
+      {errors.title && errors.title.type === "maxLength" && <p>Your title exceed maximum length</p>}
 
-        <label htmlFor='title'>Title</label>
-        <input name="title" type='text' id='title' onChange={onChangeHandler} {...register("name", { required: true, maxLength: 10 })}/>
-        {errors.title && errors.title.type === "required"&&<p>This field is required</p>}
-        {errors.title && errors.title.type === "maxLength"&&<p>Your title exceed maximum length</p>}
+      <label htmlFor='contents'>Contents</label>
+      <textarea type='text' id='contents' placeholder="200 characters or less" {...register("contents", { required: true, maxLength: 200 })} />
+      {errors.contents && errors.contents.type === "required" && <p>This field is required</p>}
+      {errors.contents && errors.contents.type === "maxLength" && <p>Your contents exceed maximum length</p>}
 
-        <label htmlFor='contents'>Contents</label>
-        <textarea name="contents" type='text' id='contents' onChange={onChangeHandler} {...register("name", { required: true, maxLength: 10 })}/>
-        {errors.contents && errors.contents.type === "required"&&<p>This field is required</p>}
-        {errors.contents && errors.contents.type === "maxLength"&&<p>Your contents exceed maximum length</p>}
-
-        <Button contents='Submit' />
-      </StForm>
-    </div>
-  );
+      <Button contents='Submit' />
+    </StForm>
+  </div>
+);
 };
 
 const StForm = styled.form`
@@ -95,6 +85,9 @@ const StForm = styled.form`
     border: none;
     font-size: 20px;
     margin-bottom: 50px;
+  }
+  p {
+    color: red;
   }
 `
 
